dfd46c849c45f738e14d7641ffcdadde51c863dd
Elias Yona <iconelias001@gmail.com>
2022-08-09 16:34:56 +0300

    Add a password reset serializer

--- django_rest_auth/registration/urls.py
+++ django_rest_auth/registration/urls.py
@@ -14,6 +14,13 @@ urlpatterns = [
     # defined just to allow reverse() call inside app, for example when email
     # with verification link is being sent, then it's required to render email
     # content.
+
+    # account_confirm_email - You should override this view to handle it in
+    # your API client somehow and then, send post to /verify-email/ endpoint
+    # with proper key.
+    # If you don't want to use API on that step, then just use ConfirmEmailView
+    # view from:
+    # django-allauth https://github.com/pennersr/django-allauth/blob/master/allauth/account/views.py
     re_path(
         r'^account-confirm-email/(?P<key>[-:\w]+)/$', TemplateView.as_view(),
         name='account_confirm_email',
--- django_rest_auth/serializers.py
+++ django_rest_auth/serializers.py
@@ -331,26 +331,26 @@ class PasswordResetConfirmSerializer(serializers.Serializer):
             from django.contrib.auth.tokens import default_token_generator
             from django.utils.http import urlsafe_base64_decode as uid_decoder
 
-            # Decode the uidb64 (alluth use base36) to uid to get User object
-            try:
-                uid = force_str(uid_decoder(attrs['uid']))
-                self.user = UserModel._default_manager.get(pk=uid)
-            except (TypeError, ValueError, OverflowError, UserModel.DoesNotExist):
-                raise ValidationError({'uid': ['Invalid value']})
+        # Decode the uidb64 (alluth use base36) to uid to get User object
+        try:
+            uid = force_str(uid_decoder(attrs['uid']))
+            self.user = UserModel._default_manager.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, UserModel.DoesNotExist):
+            raise ValidationError({'uid': ['Invalid value']})
 
-            if not default_token_generator.check_token(self.user, attrs['token']):
-                raise ValidationError({'token': ['Invalid value']})
+        if not default_token_generator.check_token(self.user, attrs['token']):
+            raise ValidationError({'token': ['Invalid value']})
 
-            self.custom_validation(attrs)
-            # Construct SetPasswordForm instance
-            self.set_password_form = self.set_password_form_class(
-                user=self.user, data=attrs,
-            )
-            if not self.set_password_form.is_valid():
-                raise serializers.ValidationError(
-                    self.set_password_form.errors)
+        self.custom_validation(attrs)
+        # Construct SetPasswordForm instance
+        self.set_password_form = self.set_password_form_class(
+            user=self.user, data=attrs,
+        )
+        if not self.set_password_form.is_valid():
+            raise serializers.ValidationError(
+                self.set_password_form.errors)
 
-            return attrs
+        return attrs
 
     def save(self):
         return self.set_password_form.save()
